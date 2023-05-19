2f55d5a2853a51a59ad7a5db3540cb63d853ab0e
SaurabhMohite14 <saurabhmohite2214@gmail.com>
2023-05-20 01:45:50 +0530

    insert

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import { MongoClient } from "mongodb";
 
 async function insertTodo()
 {
-    const uri = "mongodb://localhost:27017";
+    const uri = "mongodb://127.0.0.1:27017";
     const client = new MongoClient(uri); 
-
-    
-console.log("connection success");
-
+    const db = client.db("Project");
+    let data={message:"Hello Mongo!!"};
+ let result =await db.collection("todo").insertOne({data});
 
    await client.close();
 }
